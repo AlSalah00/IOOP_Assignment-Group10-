b7339523ee51513b3a856863e2b71aeff6d25a44
MOHAMMAD ISHRAK HASAN <TP078604@mail.apu.edu.my>
2024-07-29 20:18:04 +0800

    added supplies class

---
+++ Classes/Supplies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOOP_Assignment_Group10_.Classes
+{
+    internal class Supplies
+    {
+        private string itamName;
+        private int itamQuantity;
+        private decimal itamPrice;
+
+        public Supplies(string itamName, int itamQuantity, decimal itamPrice) 
+        {
+            this.itamName = itamName;
+            this.itamQuantity = itamQuantity;
+            this.itamPrice = itamPrice;
+        }
+
+
+    }
+}
